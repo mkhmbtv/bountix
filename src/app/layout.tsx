c2047e9836fe4d2fc7d84286d5aefcf09d82946f
Mirkhat Makhambetov <72572145+mkhmbtv@users.noreply.github.com>
2024-12-03 18:42:05 +0400

    Improve styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
+import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = localFont({
@@ -33,12 +35,19 @@ export default function RootLayout({
       >
         <header className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              <LucideKanban />
+              <h1 className="text-lg font-semibold">TicketBounty</h1>
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: "outline" })}
+            >
               Tickets
             </Link>
           </div>
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,14 @@
 import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
-import { cn } from "@/lib/utils";
 import { ticketPath } from "@/paths";
 
 const TICKET_ICONS = {
@@ -19,26 +26,27 @@ const TicketsPage = () => {
           All your tickets at one place
         </p>
       </div>
+      <Separator />
       <div className="flex flex-1 flex-col items-center gap-y-4">
         {initialTickets.map((ticket) => (
-          <div
-            key={ticket.id}
-            className="w-full max-w-[420px] rounded border border-slate-100 p-4"
-          >
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h3 className="truncate text-lg font-semibold">{ticket.title}</h3>
-            <p
-              className={cn(
-                "truncate text-sm text-slate-500",
-                ticket.status === "DONE" && "line-through",
-              )}
-            >
-              {ticket.content}
-            </p>
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2">
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <span className="truncate">{ticket.title}</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="line-clamp-3 whitespace-break-spaces">
+                {ticket.content}
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
